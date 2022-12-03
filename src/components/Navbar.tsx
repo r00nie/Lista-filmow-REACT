fd4924709dc3335cd481166d03197713797a6dfa
wackomagiciann <banasiakkacperr@gmail.com>
2022-12-03 21:49:53 +0100

    Dodanie przycisków logowania i rejestracji

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,19 +7,15 @@ const Navbar: FC<{}> = () => {
     <nav className="bg-white">
       <div className="flex md:flex-row flex-col py-6 items-center font-medium md:justify-between shadow-md w-full md:px-8 px-7 gap-10">
         <p className="font-bold text-2xl cursor-pointer">MovieList.</p>
-        <ul className="flex md:flex-row flex-col uppercase items-center gap-6">
-          <Link to="/" className="px-8 inline-block">
-            Home
-          </Link>
-          <Link to="/" className="px-8">
+        <ul className="flex md:flex-row flex-col items-center gap-6 mx-2">
+          <Link to="/" className="px-8 hover:font-bold">
             About
           </Link>
-          <Link to="/" className="px-8">
-            Service
-          </Link>
-          <Link to="/" className="px-8">
+          <Link to="/" className="px-8 hover:font-bold">
             Contact
           </Link>
+          <button className="border-none bg-transparent text-black mr-4">Sign In</button>
+          <button className="px-8 py-3">Sign Up</button>
         </ul>
         {/*Mobile Navbar*/}
         {/* <ul className="md:hidden flex bg-white w-full flex-col gap-5">
