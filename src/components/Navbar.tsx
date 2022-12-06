b224fda6bed9ce1059357143c13f383828e59574
wackomagiciann <banasiakkacperr@gmail.com>
2022-12-06 17:08:15 +0100

    Dodanie rozwiniecia i chowania listy w komponencie nawigacyjnym

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,40 +1,35 @@
-import React, {FC} from 'react'
+import React, {FC, useState} from 'react'
 import {Link} from 'react-router-dom'
-import {MenuIcon, XIcon} from '@heroicons/react/outline'
+import {MenuIcon} from '@heroicons/react/outline'
 
 const Navbar: FC<{}> = () => {
+
+  const [navbarOpen, setNavbarOpen] = useState<boolean>(true);
+
+  const handleClick = (event : any) => {
+    event.preventDefault();
+    setNavbarOpen(!navbarOpen);
+    console.log(navbarOpen);
+  }
+
   return (
     <nav className="bg-white">
       <div className="flex md:flex-row flex-col py-6 items-center font-medium md:justify-between shadow-md w-full md:px-8 px-7 gap-10">
         <p className="font-bold text-2xl cursor-pointer">MovieList.</p>
-        <ul className="flex md:flex-row flex-col items-center gap-6 mx-2">
-          <Link to="/" className="px-8 hover:font-bold">
+        <ul className={"md:flex md:flex-row items-center gap-6 mx-2" + (navbarOpen===true ? " flex flex-col " : " hidden ")}>
+          <Link to="#" className="mx-6 hover:font-bold">
             About
           </Link>
-          <Link to="/" className="px-8 hover:font-bold">
+          <Link to="#" className="mx-6 hover:font-bold">
             Contact
           </Link>
-          <button className="border-none bg-transparent text-black mr-4">Sign In</button>
-          <button className="px-8 py-3">Sign Up</button>
+          <button className="sign-in-button">Sign In</button>
+          <button className="sign-up-button">Sign Up</button>
         </ul>
-        {/*Mobile Navbar*/}
-        {/* <ul className="md:hidden flex bg-white w-full flex-col gap-5">
-          <Link to="/" className="px-3 inline-block">
-            Home
-          </Link>
-          <Link to="/" className="px-3">
-            About
-          </Link>
-          <Link to="/" className="px-3">
-            Service
-          </Link>
-          <Link to="/" className="px-3">
-            Contact
-          </Link>
-        </ul> */}
-
-        <div className="text-3xl absolute cursor-pointer md:hidden right-7 top-7">
-          <MenuIcon className="w-5"/>
+        <div className="text-3xl absolute cursor-pointer md:hidden right-5 top-5">
+          <button onClick={() => {setNavbarOpen(!navbarOpen)}}>
+            <MenuIcon className="w-5"/>
+          </button>
         </div>
       </div>
     </nav>
